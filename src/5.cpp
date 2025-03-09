8fe5ad7c6ae369fcca6bc9221a1d84b9a03bb20b
bodokejaritas <bodokejaritas@gmail.com>
2025-03-09 13:20:11 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int x = 5;
+    float y = 2.5;
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    return 0;
+}

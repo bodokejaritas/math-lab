7788b96ba6dd03f69a104157f2380cf678e313e4
bodokejaritas <bodokejaritas@gmail.com>
2025-03-29 19:55:19 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    float c = 3.5f;
+    
+    cout << "The value of a + b is: " << (a + b) << endl;
+    cout << "The value of a * b is: " << (a * b) << endl;
+    cout << "The value of a / b is: " << (float)(a / b) << endl;
+    
+    return 0;
+}

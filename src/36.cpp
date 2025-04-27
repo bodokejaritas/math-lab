631886d1d7f4f1dbe163e97fbbc544c1629a5b8f
bodokejaritas <bodokejaritas@gmail.com>
2025-04-27 02:06:04 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    double y = 2.5;
+    float z = 3.14f;
+
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    cout << "The value of z is: " << z << endl;
+
+    return 0;
+}

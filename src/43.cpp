031f45079b4713e6988f474f38e8e6359c2b1040
bodokejaritas <bodokejaritas@gmail.com>
2025-05-25 23:22:37 +0000

    Add 43.cpp

---
+++ src/43.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    float c = -2.5f;
+    
+    // Perform calculations
+    int d = a + b * c;
+    cout << "The result of a + (b * c) is: " << d << endl;
+    
+    return 0;
+}
